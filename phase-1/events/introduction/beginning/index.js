a7e2d81a7e32eb53c010ba983cf7796c50103e84
Victor <victorac2324@gmail.com>
2024-03-14 14:33:33 -0400

    added events notes

--- phase-1/events/introduction/beginning/index.js
+++ phase-1/events/introduction/beginning/index.js
@@ -17,8 +17,23 @@ Takeaways
 */
 
 // ~ the "click" event
-
+document.querySelector("#today").addEventListener("click", event => {
+    console.log("The user clicked on 'Today!'")
+})
 // ~ the "submit" event
+document.querySelector("form").addEventListener("submit", event =>{
+    event.preventDefault();
+    console.log("The form was submitted!")
+    console.log(document.querySelector("form").date);
+    const submitDate = document.querySelector("form").date.value;
+    console.log(submitDate)
 
+})
 // ~ challenges
 // 1. Add some non-click, non-submit event listener to the page. It can trigger whatever you wish!
+
+
+document.querySelector("#tomorrow").addEventListener("mouseover", event =>{
+    document.querySelector("#tomorrow").style.fontWeight = "bold"
+    
+})
--- phase-1/manipulation/beginning/index.js
+++ phase-1/manipulation/beginning/index.js
@@ -19,20 +19,31 @@ Challenges
 
 // ~ APIs, CRUD, `document`
 
-// ~ Read/Select a node
+// ~ Read/Select a node finding an exiting tag within html
 // -> querySelector is a DOM method that lets us look up nodes by CSS **selector**.
+console.log(document.querySelector("#today"))
 // -> querySelectorAll is a DOM method that returns an array-like list of nodes that match a CSS selector.
+document.querySelectorAll("p").forEach(pElement => {
+    console.log(pElement)
+});
 // -> textContent is a property of text nodes (e.g. h1, p) that contain their text.
-
+console.log(document.querySelector("#today").textContent)
 
 // ~ Update/Modify a node's attributes
 // -> To modify an attribute, just use = after the attribute name in object dot notation
+document.querySelector("#today").textContent = "Today!";
+document.querySelector("#today").style.color = "green";
 
 // ~ Delete/Remove a node
 // -> To remove an existing element, we can look it up and call the node's method `remove()`.
+document.querySelector("#tomorrow").remove();
 
 // ~ Create + Append a node
 // -> createElement(), append()
+
+const image = document.createElement("img");
+image.src = "https://thumbor.forbes.com/thumbor/fit-in/1290x/https://www.forbes.com/advisor/wp-content/uploads/2023/07/top-20-small-dog-breeds.jpeg.jpg";
+document.body.append(image)
 
 // ~ Challenges
 // 1. Write a function named displayList that takes a name and an array,
@@ -43,4 +54,22 @@ Challenges
 // 2. Replace the <strong> element with a newly created one.
 
 
+// function displayList(name, array,) {
+//     // const bName = name
+//     // const sArray= array
 
+//     //  console.log("Books", "TheShinning")
+// }
+// console.log("Books", "TheShinning")
+
+// document.querySelector("dates").append(header)
+
+// li
+
+
+function displayList(name, array){
+    let header = document.createElement("h1")
+    header.textContent = name;
+    document.querySelector("#dates").append(header)
+    
+}
